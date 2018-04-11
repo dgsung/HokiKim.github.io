8b45f655a6b1e0cd62a05a0d32a63154237f4e19
Hoki Kim <ghrl9613@snu.ac.kr>
2018-04-11 12:26:32 +0900

    Digireum

--- Digireum/Digireum.sol
+++ Digireum/Digireum.sol
@@ -35,15 +35,5 @@ contract Digimon {
     function saying(string say) {
         adrs_digivice[msg.sender].digimon_id = keccak256(say);
     }
-    
-    function upper(string _base) 
-        internal 
-        returns (string) {
-        bytes memory _baseBytes = bytes(_base);
-        for (uint i = 0; i < _baseBytes.length; i++) {
-            _baseBytes[i] = _upper(_baseBytes[i]);
-        }
-        return string(_baseBytes);
-    }
 
 }
---
+++ Digireum/upper.sol
@@ -0,0 +1,10 @@
+    
+    function upper(string _base) 
+        internal 
+        returns (string) {
+        bytes memory _baseBytes = bytes(_base);
+        for (uint i = 0; i < _baseBytes.length; i++) {
+            _baseBytes[i] = _upper(_baseBytes[i]);
+        }
+        return string(_baseBytes);
+    }
