f7416b8c3a1dc7ef8aacdb82bd9281e5f06c7d64
Hoki Kim <ghrl9613@snu.ac.kr>
2018-04-23 11:45:09 +0900

    digireum

--- Digireum/metamask.js
+++ Digireum/metamask.js
@@ -7,8 +7,8 @@
 	// Use Mist/MetaMask's provider
 		window.web3 = new Web3(web3.currentProvider);
 	} else {
-		
-		console.log('No web3? You should consider trying MetaMask!')
+		alert("Check MetaMask Login");
+		console.log('No web3? You should consider trying MetaMask!');
 	    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
 	    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 	}
